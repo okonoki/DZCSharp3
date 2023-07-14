fb531d384ec4ffa1ea03a811d219a26e2a5046f7
okonoki <okonokiss@gmail.com>
2023-07-14 09:57:37 +0500

    task3IsDone

--- task3/Program.cs
+++ task3/Program.cs
@@ -2,3 +2,25 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    Console.Clear();
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int N = Prompt("Введите число N: ");
+void NumberCubed(int N)
+{
+    for (int i = 1; i <= N; i += 1)
+    {
+        if (i < N)
+        {
+             Console.Write(i * i * i + ", ");
+        }
+        else Console.Write(i * i * i);
+    }
+}
+NumberCubed(N);
